47b4ad31a27d6d6dbdb2893abc34341f0519bb21
jonas18121 <jonasxtrm971@gmail.com>
2020-10-05 15:48:38 +0200

    menu responsive

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+///////////////// variable
+
+let menu_nav = document.querySelector('#menu_nav');
+let burger = document.querySelector('#style_burger');
+// let burger2 = document.getElementById('#style_burger');
+
+///////////////// fonction
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    // Créé un "toggle class" en Javascrit natif (compatible partout)
+    burger.addEventListener("click", function(e) {
+    
+        e.preventDefault();
+
+        if (!this.getAttribute('class')) {
+            this.setAttribute('class', 'clicked');
+        }
+        else{
+            this.removeAttribute('class');
+        }
+    
+    
+        // Créé l'effet pour le menu slide (compatible partout)
+        if (menu_nav.getAttribute('class') != 'visible') {
+            menu_nav.setAttribute('class', 'visible');
+        }
+        else{
+            menu_nav.setAttribute('class', 'invisible');
+        }
+    
+    }, false);
+});
+
+
+///////////////// programme
